fbafe8ff53fb313b84aac41e9246b07148473da3
Hwayeol <barkgitae@yahoo.com>
2015-11-23 09:57:27 +0900

    p.393

---
+++ TestJava/src/chap09/Box9Tset1.java
@@ -0,0 +1,35 @@
+package chap09;
+
+class Box9{
+	private int width;
+	private int height;
+	private int depth;
+	private int vol;
+	public Box9(int width, int height, int depth){
+		this.width = width;
+		this.height = height;
+		this.depth = depth;
+		volume();
+	}
+	private void volume(){
+		vol = width*height*depth;
+	}
+	public int getvolume(){
+		return vol;
+	}
+}
+
+public class Box9Tset1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1505101 박기태");
+		
+		Box9 mybox1 = new Box9(10,20,30);
+		//mybox1.width = 20;
+		//int vol1 = mybox1.volume();
+		System.out.println("정수 박스의 부피 : "+mybox1.getvolume());
+
+	}
+
+}
